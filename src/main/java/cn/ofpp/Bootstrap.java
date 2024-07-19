17c4abf19a845a057995b903dfa5cd66d6106091
yuchengwei <158396129@qq.com>
2024-07-19 14:16:18 +0800

    Update Bootstrap.java
    1

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx8b22ad436c903b75";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "d0407c9d89e70d2899c7503f4f66ee26";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "_1P7DHjT6TQd5j7bdxNbteAVtm1VOiaJ6cIvBxIf6oc";
 
     /**
      * 初始化
